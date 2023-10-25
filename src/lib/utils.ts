c662c8d39d5e85ca8a9a408d9301d2df2c8bf49b
hyper-neutrino <alex.yj.liao@gmail.com>
2023-10-25 11:09:02 -0400

    avoid NPE on diffing settings

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,11 +23,11 @@ export function nonempty(record: any) {
     if (Object.keys(record).length === 0) throw new APIError(400, codes.NOT_MODIFIED, "No changes were made.");
 }
 
-export function changes(before: Record<string, any>, after: Record<string, any>): Record<string, [any, any]> {
+export function changes(before: Record<string, any> | undefined, after: Record<string, any> | undefined): Record<string, [any, any]> {
     const output: Record<string, [any, any]> = {};
 
-    for (const key of Object.keys(before)) if (after[key] !== undefined && after[key] !== before[key]) output[key] = [before[key], after[key]];
-    for (const key of Object.keys(after)) if (!(key in before)) output[key] = [null, after[key]];
+    for (const key of Object.keys(before ?? {})) if (after?.[key] !== undefined && after[key] !== before![key]) output[key] = [before![key], after[key]];
+    for (const key of Object.keys(after ?? {})) if (before && !(key in before)) output[key] = [null, after![key]];
 
     return output;
 }
